5cc26f50e8736f3ab2419eee90f30522ed66fbaf
akshayxemo <akshaykrdas001@gmail.com>
2023-07-15 22:08:52 +0530

    Navbar Animation and Shadow added

--- frontend/src/layouts/Navbar/navbar.jsx
+++ frontend/src/layouts/Navbar/navbar.jsx
@@ -1,10 +1,30 @@
 import logo from "../../assets/intervuMe-original-black-logo.svg";
 import "./css/navbar.css";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 function Navbar() {
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.scrollY;
+      const threshold = 300;
+      const visible = prevScrollPos > currentScrollPos;
+
+      setVisible(currentScrollPos>threshold? visible: false);
+      setPrevScrollPos(currentScrollPos);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [prevScrollPos]);
   return (
     <>
-      <nav className="navbar">
+      <nav className={visible? "nav-sticky": "navbar"} id="nav">
         <div className="container-1280-width nav-flex">
           <Link to={`/`}>
             <img src={logo} className="nav-logo"></img>
