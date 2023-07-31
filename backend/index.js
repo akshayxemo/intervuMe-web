c32afc084d1900ccb46712e2bb1f9d2f25b248f8
akshayxemo <akshaykrdas001@gmail.com>
2023-07-31 21:03:20 +0530

    cors header

--- backend/index.js
+++ backend/index.js
@@ -20,11 +20,19 @@ const dbConnect = require("./config/db.config");
 dbConnect();
 
 // middlewares
-app.use(cors({ origin: "*" }));
-// app.use(function(req, res, next){
-//   res.header("Access-Control-Allow-Origin","*");
-//   res.header("Access-Control-Allow-Methods","GET,HEAD");
-// })
+app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,OPTIONS,POST,PUT,DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
