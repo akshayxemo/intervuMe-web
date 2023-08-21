161061b0cb1a954c8b7e58701b03d94ac62f0a94
akshayxemo <akshaykrdas001@gmail.com>
2023-08-21 12:26:33 +0530

    forget password link resolution

--- backend/controllers/forgetPasswordController.js
+++ backend/controllers/forgetPasswordController.js
@@ -34,7 +34,7 @@ module.exports = {
         );
 
         // reset password link
-        const link = `http://localhost:3000/reset-password/${foundUser._id}/${token}`;
+        const link = `${process.env.FRONTEND_API}/reset-password/${foundUser._id}/${token}`;
         console.log(link);
 
         // sending email
